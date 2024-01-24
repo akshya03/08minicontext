db28a564a9544dcb22e30b367fa10e3f320e0d70
akshya <akshya.nanda03@gmail.com>
2024-01-24 11:18:18 +0530

    mini context done

---
+++ src/context/userContext.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const UserContext = React.createContext();  //global user context
+// we will wrap around all components inside this UserContext
+
+export default UserContext;
